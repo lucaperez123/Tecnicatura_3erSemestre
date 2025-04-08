32754f43369137e70ed257725b589bf2806e025e
luca <lucamarper@gmail.com>
2025-04-08 14:34:52 -0300

    bloques else y finaly al manejar expceciones

--- python/Excepciones/Leccion01/Manejo_Excepciones.py
+++ python/Excepciones/Leccion01/Manejo_Excepciones.py
@@ -10,6 +10,12 @@ except ZeroDivisionError as e:
     print(f'ZeroDivisionError - Ocurrio un error: {type(e)}')
 except Exception as e:
     print(f'Excepcion - ocurrio un error: {type(e)}')
+else:
+    print("No se arrojo ninguna excepcion")
+finally: #Siempre se ejecuta
+    print("Ejecucion de este bloque finally")
+
+
 
 print(f'El resultado es : {resultado}')
 print('SEGUIMOS...')
