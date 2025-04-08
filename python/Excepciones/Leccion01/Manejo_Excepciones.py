db2ef7c2e346ed9248787b2ec7bc25edd9bd63d4
luca <lucamarper@gmail.com>
2025-04-08 14:42:52 -0300

    Creacion de clase exception personalizada

--- python/Excepciones/Leccion01/Manejo_Excepciones.py
+++ python/Excepciones/Leccion01/Manejo_Excepciones.py
@@ -1,9 +1,12 @@
+from NumerosIgualesException import NumerosIgualesException
 resultado = None
 
 try:
     a= int(input("Digite el primer numero: "))
     b = int(input("Digite el segundo numero: "))
-    resultado = a / b
+    if a == b:
+        raise NumerosIgualesException("Son numeros iguales")
+    resultado = a / b   
 except TypeError as e:
     print(f'TypeError - Ocurrio un error: {type(e)}')
 except ZeroDivisionError as e:
