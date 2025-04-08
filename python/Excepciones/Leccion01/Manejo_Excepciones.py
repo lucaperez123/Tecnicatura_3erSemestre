680c92d78b8297a35c977a0834cf3aaffd6e9f41
luca <lucamarper@gmail.com>
2025-04-08 14:23:37 -0300

    procesar clases de excepciones mas especificas como zero division error

--- python/Excepciones/Leccion01/Manejo_Excepciones.py
+++ python/Excepciones/Leccion01/Manejo_Excepciones.py
@@ -1,14 +1,18 @@
 resultado = None
-a= '10'
-b = 0
+a= 7
+b = 5
 
 
 
 
 try:
-    resultado = a / b 
+    resultado = a / b
+except TypeError as e:
+    print(f'TypeError - Ocurrio un error: {type(e)}')
+except ZeroDivisionError as e:
+    print(f'ZeroDivisionError - Ocurrio un error: {type(e)}')
 except Exception as e:
-    print(f'ocurrio un error: {e}')
+    print(f'Excepcion - ocurrio un error: {type(e)}')
 
 print(f'El resultado es : {resultado}')
 print('SEGUIMOS...')
