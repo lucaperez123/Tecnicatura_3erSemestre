3d63b87b43a9dae20547d13e0a46578b742c9ad1
luca <lucamarper@gmail.com>
2025-04-09 18:37:24 -0300

    Modificaciones en la clase dispositivoentrada

--- java/SistemaVentas/src/ventas/DispositivoEntrada.java
+++ java/SistemaVentas/src/ventas/DispositivoEntrada.java
@@ -1,8 +1,8 @@
 package ventas;
 
 public class DispositivoEntrada {
-    protected String tipoEntrada;
-    protected String marca;
+    private String tipoEntrada;
+    private String marca;
 
     public DispositivoEntrada(String tipoEntrada, String marca) {
         this.tipoEntrada = tipoEntrada;
