dbd086da532e5bb0a73b5d09c4c523114e211ce7
luca <lucamarper@gmail.com>
2025-04-14 18:59:46 -0300

    Especificamos el juego de caracteres con mayusculas en archivo de textos

--- python/Archivos/Leccion02/Manejo_de_Archivos.py
+++ python/Archivos/Leccion02/Manejo_de_Archivos.py
@@ -1,9 +1,10 @@
 # Declaramos una variable
 
 try:
-
-    archivo = open('prueba.txt', 'w') # La W es de write que significa escribir
+    archivo = open('prueba.txt', 'w', encoding='utf8' ) # La W es de write que significa escribir
     archivo.write('Programamos con diferentes tipos de achivos, ahora en txt. ')
+    archivo.write('Los acentos son importantes para las palabras')
+    archivo.write('Como por ejemplo: acción y producción')
     archivo.write('Con esto finalizamos')
 except Exception as e:
     print(e)
