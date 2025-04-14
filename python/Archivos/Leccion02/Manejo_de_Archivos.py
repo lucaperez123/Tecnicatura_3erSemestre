9e330b1b99d2c6ae7efecef88ea4b98ddc67c516
luca <lucamarper@gmail.com>
2025-04-14 18:47:58 -0300

    Agregamos a traves del metodo write texto en nuestro archivo txt

--- python/Archivos/Leccion02/Manejo_de_Archivos.py
+++ python/Archivos/Leccion02/Manejo_de_Archivos.py
@@ -3,9 +3,11 @@
 try:
 
     archivo = open('prueba.txt', 'w') # La W es de write que significa escribir
+    archivo.write('Programamos con diferentes tipos de achivos, ahora en txt. ')
+    archivo.write('Con esto finalizamos')
 except Exception as e:
     print(e)
 finally: # Siempre se ejecuta    
     archivo.close() #Con esto se debe cerrar el archivo
 
-    
+
