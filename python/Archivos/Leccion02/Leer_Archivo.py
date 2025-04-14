f7c5d22d7aa53e0037790d75cfca6a35da77c6bd
luca <lucamarper@gmail.com>
2025-04-14 19:34:07 -0300

    Mas formas de manejo de archivos

--- python/Archivos/Leccion02/Leer_Archivo.py
+++ python/Archivos/Leccion02/Leer_Archivo.py
@@ -1,3 +1,13 @@
 
-achivo = open("prueba.txt", 'r', encoding="utf8")
+archivo = open("prueba.txt", 'r', encoding="utf8") #Las letras son = 'r' , 'a' , 'w' , 'x'
 print(archivo.read())
+print(archivo.read(16))
+print(archivo.read(10)) #Continuamos desde la linea anterior
+print(archivo.readLine())
+print(archivo.readLine())
+
+
+#vamos a iterar el archivo , cada una de sus lineas
+for linea in archivo:
+    print(linea)
+ 
