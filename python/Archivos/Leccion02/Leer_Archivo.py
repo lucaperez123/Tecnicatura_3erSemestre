818a1d6d5e50da66ce6ef73cdb32f08c4028b73c
luca <lucamarper@gmail.com>
2025-04-14 19:39:02 -0300

    Anexamos informacion de archivo1 a un nuevo archivo

--- python/Archivos/Leccion02/Leer_Archivo.py
+++ python/Archivos/Leccion02/Leer_Archivo.py
@@ -8,6 +8,14 @@ print(archivo.readLine())
 
 
 #vamos a iterar el archivo , cada una de sus lineas
-for linea in archivo:
-    print(linea)
- 
+#for linea in archivo:
+    #print(linea) iteramos todos los elementos del archivo
+    #print(archivo.readlines()[11]) accedemos al archivo como si fuera una lista
+
+#Anexamos informacion , copiamos a otro
+archivo2 = open('Copia.txt', 'a' , encoding='utf8')
+archivo2.write(archivo.read())
+archivo.close()
+archivo2.close()
+
+print('se ha terminado el proceso de leer y copiar arhcivos')
