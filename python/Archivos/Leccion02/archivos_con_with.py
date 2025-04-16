c0b8b00ae74b87b7638f2d5d5c62ffad8ace2592
luca <lucamarper@gmail.com>
2025-04-16 14:09:50 -0300

    Vimos el modo estricto en javascript

--- python/Archivos/Leccion02/archivos_con_with.py
+++ python/Archivos/Leccion02/archivos_con_with.py
@@ -1,9 +1,15 @@
+from ManejoArchivos import ManejoArchivos
+
 #MANEJO DE CONTEXTOS WITH: sintaxis simplificada , abre y cierra el archivo
 
-with open('prueba.txt' , 'r' , encoding='utf8') as archivo:
-    print(archivo.read())
+#with open('prueba.txt' , 'r' , encoding='utf8') as archivo:
+    #print(archivo.read())
 
 #no hace falta ni el try, ni el finally
 # en el contexto de with lo que se ejectura de manera automatica
 # utiliza diferentes metodos __enter__ es el que abre
 # ahora el siguiente metodo es el que cierra __exit__
+
+
+with ManejoArchivos(prueba.txt) as archivo:
+    print(archivo.read())
